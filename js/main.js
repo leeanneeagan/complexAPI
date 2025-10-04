563ab1307b13406fd8d2e4a231d0ce231d9760b7
leeanneeagan <leeanne.eagan@gmail.com>
2025-10-04 17:53:23 -0400

    finished js

--- js/main.js
+++ js/main.js
@@ -1,55 +1,95 @@
-// # 📊 Project: Simple API 1
+// //complex API
 
-// ### Goal: Display data returned from an api
+// //find api.     https://musicbrainz.org/ws/2/artist?query=drake&fmt=json
+// //find second api https://api.lyrics.ovh/v1/${artist}/NOKIA`
+// //getkey.       doesnt require key
+// // run it through postman.co     came back as 200 ok
+// //build event listener 
+// //make function 
+// // call function and set variable to select parameters
+// //console log to make sure it works.
+// //create a fetch and json return
+// //console log data to decide what parameters can pull
+// //console log individual ones
+// //catch to return errors 
 
+// document.querySelector('button').onclick = songSearch
+// // document.querySelector('button').onclick =
 
-
-
-//find api.     https://api.thedogapi.com/v1
-//getkey.       doesnt require key
-// run it through postman.co     came back as 200 ok
-//build event listener 
-//make function 
-// call function and set variable to select parameters
-//console log to make sure it works.
-//create a fetch and json return
-//console log data to decide what parameters can pull
-//console log individual ones
-//catch to return errors 
-
-document.querySelector('button').onclick = dogSearch
-// document.querySelector('button').onclick = dogImgSearch
-
-function dogSearch() {
+// function songSearch() {
 
 
-    const input = document.querySelector('input').value;
-    const url = `https://api.thedogapi.com/v1/breeds/search?q=${input}`;
-
-    fetch(url)
-        .then((res) => res.json())
-        .then((data => {
-            console.log(data);
-
-            document.querySelector('h2').innerText = data[0].name;
-            document.querySelector('.breedGroup').innerText = data[0].breed_group;
-            document.querySelector('.height').innerText = data[0].height.metric.split(" - ").map(v => (v * 0.393701).toFixed(1)).join(" - ") + " in";//used chatGpt for forumla conversion
-            document.querySelector('.weight').innerText = data[0].weight.metric.split(" - ").map(v => (v * 2.20462).toFixed(1)).join(" - ") + " lbs";//^^ ^^ ^^
-            document.querySelector('.lifeSpan').innerText = data[0].life_span;
-            document.querySelector('.mood').innerText = data[0].temperament;
-
-        }))
-        .catch((err) => console.error(err));
-}
-// function dogImgSearch() {
-//     const input = document.querySelector('input').value;
-//     const url = `"https://api.thedogapi.com/v1/images/search"?q=${input}`;
+//     let artist = document.querySelector('input').value;
+//     const url = `https://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json`;
 
 //     fetch(url)
 //         .then((res) => res.json())
-//         .then((data) => {
-//             console.log(data); // Array of breeds matching the input
-//         })
-//         .catch((err) => console.error(err));
+//         .then((data => {
+//             console.log(data);
+
+//             document.querySelector('h4').innerText = data.artists[0].aliases[1].name;
+
+//             let nameOfArtist = data.artists[0]
+//             findSongs(nameOfArtist)
+//         }))
+//         .catch(err => console.error(err));
 // }
 
+// function findSongs(nameOfArtist) {
+//     const songUrl = `https://itunes.apple.com/search?term=${nameOfArtist}&entity=song&limit=5`
+//     fetch(songUrl)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data);
+//         document.querySelector('h4').innerText = data.artists[0].aliases[1].name;
+
+
+
+//         })
+//         .catch(err => console.error(err));
+// }
+
+
+
+
+///used chat gpt to see why my first bloack of code above was not pulling songs 
+// from 'input' artist to second function,
+//  and the result was the first function was passing [object obect] 
+// into song find in second function 
+// after review i was able to understand that 
+// it was pulling the artist alias i set in my first fetch / print to DOM 
+// into song find instead of artist name 
+// 
+document.querySelector('button').onclick = songSearch;
+
+function songSearch() {
+    let artist = document.querySelector('input').value.trim();
+    const url = `https://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+
+            const artistName = data.artists[0].name;
+            document.querySelector('h4').innerText = data.artists[0].aliases[1].name;
+
+            findSongs(artistName);
+        })
+        .catch(err => console.error("Error fetching artist:", err));
+}
+
+function findSongs(artistName) {
+    const songUrl = `https://itunes.apple.com/search?term=${encodeURIComponent(artistName)}&entity=song&limit=5`;
+
+    fetch(songUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            // Display song names just to confirm it’s working
+            const songs = data.results.map(song => song.trackName).join('\n');
+            document.querySelector('h5').innerText = `Songs by ${artistName}:\n${songs}`;
+        })
+        .catch(err => console.error("Error fetching songs:", err));
+}
